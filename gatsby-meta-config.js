d0078f691a6cfd1c82214ee7159a24c518c4bfe4
Wansik Jo <jws5327@ajou.ac.kr>
2021-12-04 00:28:18 +0900

    Init Blog

---
+++ gatsby-meta-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: `41312432`,
+  description: `WansikJo Devlog`,
+  author: `Jo WanSik`,
+  introduction: `Studying Software & Computer Engineering at @Ajou Univ.`,
+  siteUrl: `https://41312432.github.io`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `41312432`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '41312432-github-io.disqus.com', // Your disqus-short-name. check disqus.com.
+    utterances: '41312432/41312432.github.io', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+  ad: '', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
+}
