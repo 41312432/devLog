39bdd93f3f06930d673dbf4d4c5271bf5bb05569
Wansik Jo <jws5327@ajou.ac.kr>
2021-12-12 22:23:51 +0900

    STYLE: Update theme-change location

--- src/components/top/index.jsx
+++ src/components/top/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import { ThemeSwitch } from '../theme-switch'
+
 import './index.scss'
 
 export const Top = ({ title, location, rootPath }) => {
   const isRoot = location.pathname === rootPath
   return (
     <div className="top">
-      {!isRoot && (
-        <Link to={`/`} className="link">
-          {title}
-        </Link>
-      )}
+      <Link to={`/`} className="link">
+        {title}
+      </Link>
+      <ThemeSwitch />
     </div>
   )
 }
