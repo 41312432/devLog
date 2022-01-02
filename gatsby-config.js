4674931c88b061954172ab0a1168348019b02cb8
Wansik Jo <jws5327@ajou.ac.kr>
2022-01-02 23:47:43 +0900

    FEAT: Make header padding top to fit header
    GIve header padding-top to apply toc autolink to fit top
    Implement in this way because gatsby plugin remark-autolink-header's
    offset option does not operate

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,6 +62,16 @@ module.exports = {
             },
           },
           {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              offsetY: `60`,
+              className: `anchor-header`, // 이 class명으로 하이라이트 코드를 구현할 예정이므로 반드시 넣자.
+              maintainCase: false, // 이 부분은 반드시 false로 하자. url이 대소문자를 구분하기 때문에 링크가 작동하지 않을 수 있다.
+              removeAccents: true,
+              elements: [`h1`, `h2`, 'h3', 'h4', 'h5', 'h6'], // 링크를 추가할 Header 종류 선택
+            },
+          },
+          {
             resolve: `gatsby-remark-prismjs`,
             options: {
               classPrefix: 'language-',
@@ -69,15 +79,6 @@ module.exports = {
               aliases: {},
               showLineNumbers: false,
               noInlineHighlight: false,
-            },
-          },
-          {
-            resolve: `gatsby-remark-autolink-headers`,
-            options: {
-              className: `anchor-header`, // 이 class명으로 하이라이트 코드를 구현할 예정이므로 반드시 넣자.
-              maintainCase: false, // 이 부분은 반드시 false로 하자. url이 대소문자를 구분하기 때문에 링크가 작동하지 않을 수 있다.
-              removeAccents: true,
-              elements: [`h1`, `h2`, 'h3', 'h4', 'h5', 'h6'], // 링크를 추가할 Header 종류 선택
             },
           },
           `gatsby-remark-copy-linked-files`,
